60524d38bab6ba4ea3ac3d50911e94d59c4b1c0f
Martin Trenkmann <martin.trenkmann@posteo.de>
2016-09-24 00:46:12 +0200

    Added Javadoc

--- src/io/phrasefinder/PhraseFinder.java
+++ src/io/phrasefinder/PhraseFinder.java
@@ -27,66 +27,180 @@ import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Class PhraseFinder provides routines for querying the <a
+ * href="http://phrasefinder.io">PhraseFinder</a> web service. Class <a
+ * href="https://github.com/mtrenkmann/phrasefinder-client-java/blob/master/src/Demo.java"
+ * >Demo</a> shows a usage example.
+ */
 public final class PhraseFinder {
   
+  /**
+   * Corpus is a numeric type that represents a corpus to be searched. All corpora belong to version
+   * 2 of the <a href="http://storage.googleapis.com/books/ngrams/books/datasetsv2.html"> Google
+   * Books Ngram Dataset</a>.
+   */
   public static enum Corpus {
-    ENGLISH_US,  // 0
-    ENGLISH_GB,  // 1
-    SPANISH,     // 2
-    FRENCH,      // 3
-    GERMAN,      // 4
-    RUSSIAN,     // 5
-    CHINESE      // 6
+    ENGLISH_US, ENGLISH_GB, SPANISH, FRENCH, GERMAN, RUSSIAN,
+    /** Simplified Chinese */
+    CHINESE
   }
   
+  /**
+   * Status is a type that reports whether a request was successful or not. The value is derived
+   * from the HTTP status code sent along with a response. Note that the numeric value does not
+   * correspond to the original HTTP code.
+   */
   public static enum Status {
-    OK,                  // 0
-    BAD_REQUEST,         // 1
-    PAYMENT_REQUIRED,    // 2
-    METHOD_NOT_ALLOWED,  // 3
-    TOO_MANY_REQUESTS,   // 4
-    SERVER_ERROR         // 5
+    /** The request was valid and the response body contains the result. */
+    OK,
+
+    /**
+     * A required parameter was missing or a parameter had an invalid value (wrong string or
+     * out-of-range number).
+     */
+    BAD_REQUEST,
+
+    /**
+     * The request contained an API key, but that key was either unknown or the associated quota has
+     * been consumed already.
+     */
+    PAYMENT_REQUIRED,
+
+    /** The request method was something other than GET. */
+    METHOD_NOT_ALLOWED,
+
+    /**
+     * The request did not contain an API key and the daily free quota associated with the IP
+     * address has been consumed already.
+     */
+    TOO_MANY_REQUESTS,
+
+    /** The server needs a break. Please try again later. */
+    SERVER_ERROR
   }
   
+  /**
+   * Token represents a single token (word, punctuation mark, etc.) as part of a phrase.
+   */
   public static class Token {
+
+    /**
+     * TokenTag denotes the role of a token with respect to the query.
+     */
     public static enum Tag {
-      GIVEN,        // 0
-      INSERTED,     // 1
-      ALTERNATIVE,  // 2
-      COMPLETED     // 3
+      /** The token was given as part of the query. */
+      GIVEN,
+
+      /**
+       * The token has been inserted either by an application of the "?" or the "*" operator.
+       */
+      INSERTED,
+
+      /** The token was given as the left- or right-hand side of the "/" operator. */
+      ALTERNATIVE,
+
+      /** The token has been completed by an application of the "+" operator. */
+      COMPLETED
     }
+
     public String text;
-    public Tag    tag;
+    public Tag tag;
   }
   
+  /**
+   * Phrase represents a phrase, also called n-gram. A phrase consists of a sequence of tokens and
+   * metadata.
+   */
   public static class Phrase {
+    /** The tokens of the phrase. */
     public Token[] tokens;
-    public long    matchCount;
-    public int     volumeCount;
-    public int     firstYear;
-    public int     lastYear;
-    public int     relativeId;
-    public double  score;
+
+    /** The absolute frequency in the corpus. */
+    public long matchCount;
+
+    /** The number of books it appears in. */
+    public int volumeCount;
+
+    /** Publication date of the first book it appears in. */
+    public int firstYear;
+
+    /** Publication date of the last book it appears in. */
+    public int lastYear;
+
+    /** See the API documentation on the website. */
+    public int relativeId;
+
+    /** The relative frequency it matched the given query. */
+    public double score;
   }
   
+  /**
+   * Options represents optional parameters that can be sent along with a query.
+   */
   public static class Options {
+    /** The corpus to be searched. */
     public Corpus corpus = Corpus.ENGLISH_US;
-    public String key    = null;
-    public int    nmin   = 1;
-    public int    nmax   = 5;
-    public int    topk   = 100;
+
+    /**
+     * The minimum length of matching phrases (number of tokens) to be included in the result set.
+     */
+    public int nmin = 1;
+
+    /**
+     * The maximum length of matching phrases (number of tokens) to be included in the result set.
+     */
+    public int nmax = 5;
+
+    /**
+     * The maximum number of phrases to be returned. A smaller value may lead to slightly faster
+     * responses.
+     */
+    public int topk = 100;
+
+    /**
+     * The API key if you own one. Requests without an API key will be accounted to the daily free
+     * quota associated with the IP address of the client.
+     */
+    public String key = null;
   }
   
+  /**
+   * Result represents a search result.
+   */
   public static class Result {
-    public Status   status;
+    /** The status of the associated response. */
+    public Status status;
+
+    /** A list of matching phrases. */
     public Phrase[] phrases;
-    public int      quota;
+
+    /** The remaining number of requests. */
+    public int quota;
   }
   
+  /**
+   * Sends a request to the server with all optional parameters set to default.
+   * 
+   * @param query is the query string.
+   * @return A {@link Result} object whose {@link Result#status} field is equal to {@link Status#OK}
+   *         if the request was successful. In this case other fields of the object are in valid
+   *         state and can be read. Any status other than {@link Status#OK} indicates a failed
+   *         request. In that case other fields in the result have unspecified data.
+   * @throws IOException when sending the request or receiving the response failed.
+   */
   public static Result search(String query) throws IOException {
     return search(query, new Options());
   }
   
+  /**
+   * Sends a request to the server with parameters.
+   * 
+   * @param query is the query string.
+   * @param options are the parameters.
+   * @return Same as {@link #search(String)}
+   * @throws IOException Same as {@link #search(String)}
+   */
   public static Result search(String query, Options options) throws IOException {
     HttpURLConnection connection = (HttpURLConnection) toUrl(query, options).openConnection();
     Result response = new Result();
