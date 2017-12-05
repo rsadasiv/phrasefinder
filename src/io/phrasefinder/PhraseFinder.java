822a2963b67a906bd1b6bc6b52107485a6c665ba
Martin Trenkmann <martin.trenkmann@posteo.de>
2017-12-05 12:27:18 +0100

    Formatting

--- src/io/phrasefinder/PhraseFinder.java
+++ src/io/phrasefinder/PhraseFinder.java
@@ -1,12 +1,12 @@
-// This file is part of PhraseFinder.  http://phrasefinder.io
+// This file is part of PhraseFinder. http://phrasefinder.io
 //
-// Copyright (C) 2016-2017  Martin Trenkmann
+// Copyright (C) 2016-2017 Martin Trenkmann
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//    http://www.apache.org/licenses/LICENSE-2.0
+// http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -28,32 +28,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Class PhraseFinder provides routines for querying the <a
- * href="http://phrasefinder.io">PhraseFinder</a> web service. Class <a
- * href="https://github.com/mtrenkmann/phrasefinder-client-java/blob/master/src/Demo.java"
+ * Class PhraseFinder provides routines for querying the
+ * <a href="http://phrasefinder.io">PhraseFinder</a> web service. Class
+ * <a href="https://github.com/mtrenkmann/phrasefinder-client-java/blob/master/src/Demo.java"
  * >Demo</a> shows a usage example.
  */
 public final class PhraseFinder {
-  
+
   /**
    * The major version number.
    */
   public static int VERSION_MAJOR = 0;
-  
+
   /**
    * The minor version number.
    */
   public static int VERSION_MINOR = 2;
-  
+
   /**
    * The build version number.
    */
   public static int VERSION_BUILD = 0;
-  
+
   /**
    * Defines the version number as one integer.
    */
-  public static int VERSION = VERSION_MAJOR * 10^6 + VERSION_MINOR * 10^3 + VERSION_BUILD;
+  public static int VERSION = VERSION_MAJOR * 10 ^ 6 + VERSION_MINOR * 10 ^ 3 + VERSION_BUILD;
 
   /**
    * Corpus is a numeric type that represents a corpus to be searched. All corpora belong to version
@@ -63,14 +63,16 @@ public final class PhraseFinder {
   public static enum Corpus {
     AMERICAN_ENGLISH, BRITISH_ENGLISH, CHINESE, FRENCH, GERMAN, RUSSIAN, SPANISH
   }
-  
+
   /**
    * Status is a type that reports whether a request was successful or not. The value is derived
    * from the HTTP status code sent along with a response. Note that the numeric value does not
    * correspond to the original HTTP code.
    */
   public static enum Status {
-    /** The request was successful and the response body contains the result. **/
+    /**
+     * The request was successful and the response body contains the result.
+     */
     OK,
 
     /**
@@ -79,13 +81,17 @@ public final class PhraseFinder {
      */
     BAD_REQUEST,
 
-    /** The request method was something other than GET. */
+    /**
+     * The request method was something other than GET.
+     */
     METHOD_NOT_ALLOWED,
 
-    /** The server needs a break. Please try again later. */
+    /**
+     * The server needs a break. Please try again later.
+     */
     SERVER_ERROR
   }
-  
+
   /**
    * Token represents a single token (word, punctuation mark, etc.) as part of a phrase.
    */
@@ -95,7 +101,9 @@ public final class PhraseFinder {
      * Tag denotes the role of a token with respect to the query.
      */
     public static enum Tag {
-      /** The token was given as part of the query. */
+      /**
+       * The token was given as part of the query.
+       */
       GIVEN,
 
       /**
@@ -103,16 +111,20 @@ public final class PhraseFinder {
        */
       INSERTED,
 
-      /** The token was given as the left- or right-hand side of the "/" operator. */
+      /**
+       * The token was given as the left- or right-hand side of the "/" operator.
+       */
       ALTERNATIVE,
 
-      /** The token has been completed by an application of the "+" operator. */
+      /**
+       * The token has been completed by an application of the "+" operator.
+       */
       COMPLETED
     }
-    
+
     private Tag tag;
     private String text;
-    
+
     public Token(Tag tag, String text) {
       this.tag = tag;
       this.text = text;
@@ -124,7 +136,7 @@ public final class PhraseFinder {
     public Tag getTag() {
       return tag;
     }
-    
+
     /**
      * Returns the token's text.
      */
@@ -133,39 +145,55 @@ public final class PhraseFinder {
     }
 
   }
-  
+
   /**
    * Phrase represents a phrase, also called n-gram. A phrase consists of a sequence of tokens and
    * metadata.
    */
   public static class Phrase {
-    /** The tokens of the phrase. */
+    /**
+     * The tokens of the phrase.
+     */
     public Token[] tokens;
 
-    /** The absolute frequency in the corpus. */
+    /**
+     * The absolute frequency in the corpus.
+     */
     public long matchCount;
 
-    /** The number of books it appears in. */
+    /**
+     * The number of books it appears in.
+     */
     public int volumeCount;
 
-    /** Publication date of the first book it appears in. */
+    /**
+     * Publication date of the first book it appears in.
+     */
     public int firstYear;
 
-    /** Publication date of the last book it appears in. */
+    /**
+     * Publication date of the last book it appears in.
+     */
     public int lastYear;
 
-    /** See the API documentation on the website. */
+    /**
+     * See the API documentation on the website.
+     */
     public int relativeId;
 
-    /** The relative frequency it matched the given query. */
+    /**
+     * The relative frequency it matched the given query.
+     */
     public double score;
   }
-  
+
   /**
    * Options represents optional parameters that can be sent along with a query.
    */
   public static class Options {
-    /** The corpus to be searched. */
+    /**
+     * The corpus to be searched.
+     */
     public Corpus corpus = Corpus.AMERICAN_ENGLISH;
 
     /**
@@ -184,18 +212,22 @@ public final class PhraseFinder {
      */
     public int topk = 100;
   }
-  
+
   /**
    * Result represents a search result.
    */
   public static class Result {
-    /** The status of the associated response. */
+    /**
+     * The status of the associated response.
+     */
     public Status status;
 
-    /** A list of matching phrases. */
+    /**
+     * A list of matching phrases.
+     */
     public Phrase[] phrases;
   }
-  
+
   /**
    * Sends a request to the server with all optional parameters set to default.
    * 
@@ -209,7 +241,7 @@ public final class PhraseFinder {
   public static Result search(String query) throws IOException {
     return search(query, new Options());
   }
-  
+
   /**
    * Sends a request to the server with parameters.
    * 
@@ -223,8 +255,8 @@ public final class PhraseFinder {
     Result response = new Result();
     response.status = toStatus(connection.getResponseCode());
     if (response.status == Status.OK) {
-      try (BufferedReader reader = new BufferedReader(
-          new InputStreamReader(connection.getInputStream(),
+      try (BufferedReader reader =
+          new BufferedReader(new InputStreamReader(connection.getInputStream(),
               java.nio.charset.StandardCharsets.UTF_8))) {
         String line = null;
         List<Phrase> phrases = new ArrayList<>();
@@ -235,16 +267,15 @@ public final class PhraseFinder {
           phrase.tokens = new Token[tokens.length];
           for (int i = 0; i != tokens.length; ++i) {
             int length = tokens[i].length();
-            phrase.tokens[i] = new Token(
-                toTag(Integer.parseInt(tokens[i].substring(length - 1))),
+            phrase.tokens[i] = new Token(toTag(Integer.parseInt(tokens[i].substring(length - 1))),
                 tokens[i].substring(0, length - 2));
           }
-          phrase.matchCount  = Long.parseLong(parts[1]);
+          phrase.matchCount = Long.parseLong(parts[1]);
           phrase.volumeCount = Integer.parseInt(parts[2]);
-          phrase.firstYear   = Integer.parseInt(parts[3]);
-          phrase.lastYear    = Integer.parseInt(parts[4]);
-          phrase.relativeId  = Integer.parseInt(parts[5]);
-          phrase.score       = Double.parseDouble(parts[6]);
+          phrase.firstYear = Integer.parseInt(parts[3]);
+          phrase.lastYear = Integer.parseInt(parts[4]);
+          phrase.relativeId = Integer.parseInt(parts[5]);
+          phrase.score = Double.parseDouble(parts[6]);
           phrases.add(phrase);
         }
         response.phrases = phrases.toArray(new Phrase[0]);
@@ -255,45 +286,55 @@ public final class PhraseFinder {
 
   private static Status toStatus(int httpResponseCode) {
     switch (httpResponseCode) {
-      case 200: return Status.OK;
-      case 400: return Status.BAD_REQUEST;
-      case 405: return Status.METHOD_NOT_ALLOWED;
-      case 500: return Status.SERVER_ERROR;
-      default:  throw new IllegalArgumentException();
+      case 200:
+        return Status.OK;
+      case 400:
+        return Status.BAD_REQUEST;
+      case 405:
+        return Status.METHOD_NOT_ALLOWED;
+      case 500:
+        return Status.SERVER_ERROR;
+      default:
+        throw new IllegalArgumentException();
     }
   }
-  
+
   private static String toString(Corpus corpus) {
     switch (corpus) {
-      case AMERICAN_ENGLISH: return "eng-us";
-      case BRITISH_ENGLISH: return "eng-gb";
-      case SPANISH:    return "spa";
-      case FRENCH:     return "fre";
-      case GERMAN:     return "ger";
-      case RUSSIAN:    return "rus";
-      case CHINESE:    return "chi";
-      default:         throw new IllegalArgumentException();
+      case AMERICAN_ENGLISH:
+        return "eng-us";
+      case BRITISH_ENGLISH:
+        return "eng-gb";
+      case CHINESE:
+        return "chi";
+      case FRENCH:
+        return "fre";
+      case GERMAN:
+        return "ger";
+      case RUSSIAN:
+        return "rus";
+      case SPANISH:
+        return "spa";
+      default:
+        throw new IllegalArgumentException();
     }
   }
-  
+
   private static Token.Tag toTag(int value) {
     if (value < Token.Tag.values().length) {
       return Token.Tag.values()[value];
     }
     throw new IllegalArgumentException();
   }
-  
+
   private static URL toUrl(String query, Options options)
       throws UnsupportedEncodingException, MalformedURLException {
     StringBuilder sb = new StringBuilder();
-    sb.append("http://phrasefinder.io/search?format=tsv")
-      .append("&query=").append(URLEncoder.encode(query,
-          java.nio.charset.StandardCharsets.UTF_8.toString()))
-      .append("&corpus=").append(toString(options.corpus))
-      .append("&nmin=").append(options.nmin)
-      .append("&nmax=").append(options.nmax)
-      .append("&topk=").append(options.topk);
+    sb.append("http://phrasefinder.io/search?format=tsv").append("&query=")
+        .append(URLEncoder.encode(query, java.nio.charset.StandardCharsets.UTF_8.toString()))
+        .append("&corpus=").append(toString(options.corpus)).append("&nmin=").append(options.nmin)
+        .append("&nmax=").append(options.nmax).append("&topk=").append(options.topk);
     return new URL(sb.toString());
   }
-  
+
 }
