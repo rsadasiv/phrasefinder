a3e62ff1bf219fb3167e0ec7f657caa64fe63535
Martin Trenkmann <martin.trenkmann@posteo.de>
2017-12-07 13:43:52 +0100

    Formatting

--- src/test/java/io/phrasefinder/PhraseFinderTest.java
+++ src/test/java/io/phrasefinder/PhraseFinderTest.java
@@ -1,16 +1,24 @@
-/**
- * 
- */
+// This file is part of PhraseFinder. http://phrasefinder.io
+//
+// Copyright (C) 2016-2017 Martin Trenkmann
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package io.phrasefinder;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Test;
 
-/**
- * @author martin
- *
- */
 class PhraseFinderTest {
 
   /**
@@ -22,7 +30,8 @@ class PhraseFinderTest {
   }
 
   /**
-   * Test method for {@link io.phrasefinder.PhraseFinder#search(java.lang.String, io.phrasefinder.PhraseFinder.Params)}.
+   * Test method for
+   * {@link io.phrasefinder.PhraseFinder#search(java.lang.String, io.phrasefinder.PhraseFinder.Params)}.
    */
   @Test
   void testSearchStringParams() {
