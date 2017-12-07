368c9d4efcc070fedbf1eaf01802b1d25ed9e873
Martin Trenkmann <martin.trenkmann@posteo.de>
2017-12-07 13:37:38 +0100

    Initial commit

---
+++ src/test/java/io/phrasefinder/PhraseFinderTest.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package io.phrasefinder;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author martin
+ *
+ */
+class PhraseFinderTest {
+
+  /**
+   * Test method for {@link io.phrasefinder.PhraseFinder#search(java.lang.String)}.
+   */
+  @Test
+  void testSearchString() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link io.phrasefinder.PhraseFinder#search(java.lang.String, io.phrasefinder.PhraseFinder.Params)}.
+   */
+  @Test
+  void testSearchStringParams() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link java.lang.Object#hashCode()}.
+   */
+  @Test
+  void testHashCode() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link java.lang.Object#equals(java.lang.Object)}.
+   */
+  @Test
+  void testEquals() {
+    fail("Not yet implemented");
+  }
+
+}
