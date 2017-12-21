c87cf65a9e598d2ac374151ad048512c73264894
Martin Trenkmann <martin.trenkmann@posteo.de>
2017-12-21 18:00:12 +0100

    Added test to check corpus id encoding

--- src/test/java/io/phrasefinder/PhraseFinderTest.java
+++ src/test/java/io/phrasefinder/PhraseFinderTest.java
@@ -16,14 +16,19 @@
 
 package io.phrasefinder;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
 
 import org.junit.jupiter.api.Test;
 
+import io.phrasefinder.Result.Status;
+
 class PhraseFinderTest {
-  
+
+  private static final String COMMON_QUERY = "* hello *";
+
   private static final String ENG_US_QUERY = "I struggled ???";
   private static final String ENG_US_EXPECTED_PHRASE_0 = "I struggled to my feet";
   private static final String ENG_US_EXPECTED_PHRASE_1 = "I struggled to keep my";
@@ -47,6 +52,32 @@ class PhraseFinderTest {
     assertEquals(ENG_US_EXPECTED_PHRASE_0, result.getPhrases()[0].toString());
     assertEquals(ENG_US_EXPECTED_PHRASE_1, result.getPhrases()[1].toString());
     assertEquals(ENG_US_EXPECTED_PHRASE_2, result.getPhrases()[2].toString());
+  }
+
+  @Test
+  void phraseIdEncodesCorpusId() throws IOException {
+    for (Corpus corpus : Corpus.values()) {
+      Result result = PhraseFinder.search(corpus, COMMON_QUERY);
+      assertEquals(Status.OK, result.getStatus());
+      if (corpus == Corpus.RUSSIAN) {
+        // Russian does not have matching phrases for COMMON_QUERY.
+        assertEquals(0, result.getPhrases().length);
+      } else {
+        assertTrue(result.getPhrases().length > 0);
+        for (Phrase phrase : result.getPhrases()) {
+          int corpusId = Helper.getCorpusIdFromPhraseId(phrase.getId());
+          assertEquals(corpus, Corpus.fromOrdinal(corpusId));
+        }
+      }
+    }
+  }
+
+  private static class Helper {
+
+    public static int getCorpusIdFromPhraseId(long phraseId) {
+      return (int) (phraseId >>> 40);
+    }
+
   }
 
 }
