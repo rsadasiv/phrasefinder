deef56095a4c2ba6b5fbdab5bf70bfc69292568a
Martin Trenkmann <martin.trenkmann@posteo.de>
2017-12-24 14:33:12 +0100

    Updated Javadoc

--- src/main/java/io/phrasefinder/Phrase.java
+++ src/main/java/io/phrasefinder/Phrase.java
@@ -137,9 +137,10 @@ public class Phrase {
   protected Phrase() {}
 
   /**
-   * Returns an empty phrase with zero tokens and all metadata set to zero. The empty phrase can be
-   * used instead of {@code null} to represent the absent of a phrase and to reduce null checks in
-   * client code. The instance returned is a singleton, i.e. it is always the same object.
+   * Returns an empty phrase with a zero-size token array and all metadata set to zero. The empty
+   * phrase can be used instead of {@code null} to represent the absence of a phrase and to reduce
+   * null checks in client code. The instance returned is a singleton, i.e. it is always the same
+   * object.
    */
   public static Phrase getEmptyPhrase() {
     return EMPTY_PHRASE;
