da2dc00f22dd327d6cecd9b47660fa173283147b
Martin Trenkmann <martin.trenkmann@posteo.de>
2018-01-02 19:30:53 +0100

    Added test case

--- src/test/java/io/phrasefinder/SearchOptionsTest.java
+++ src/test/java/io/phrasefinder/SearchOptionsTest.java
@@ -53,4 +53,9 @@ class SearchOptionsTest {
     });
   }
 
+  @Test
+  void defaultInstanceEqualsDefaultConstructedInstance() {
+    assertEquals(SearchOptions.defaultInstance(), new SearchOptions());
+  }
+
 }
